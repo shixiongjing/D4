ae27d67ff4b36ea7e916bc0c42308c62084ffd5d
shixiongjing <shj40@pitt.edu>
2018-12-05 00:38:47 -0500

    fsdjkfasedjk

--- word_finder.rb
+++ word_finder.rb
@@ -4,14 +4,11 @@ require_relative 'helper'
 
 exit 1 unless check_args ARGV
 t = Trie.read('newlist')
-file = File.open(ARGV[0], "r")
-net = Net.new(file,t)
+net = Net.new(ARGV[0],t)
 list = net.find_word
 puts 'RESULT >>>'
 dict = read_dictionary
 for x in list
 	puts dict[x]
 end
-
-
 
